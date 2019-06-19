e4b34ceb3ddfd1067ad4f237e854a12368cbe941
David Rees <david.rees.dr@gmail.com>
2019-06-19 12:41:08 +1000

    Convert plugin to ObjC

--- ios/Classes/GetIpPlugin.m
+++ ios/Classes/GetIpPlugin.m
@@ -1,8 +1,68 @@
 #import "GetIpPlugin.h"
-#import <get_ip/get_ip-Swift.h>
 
 @implementation GetIpPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftGetIpPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel =
+    [FlutterMethodChannel methodChannelWithName:@"get_ip"
+                                binaryMessenger:[registrar messenger]];
+    GetIpPlugin* instance = [[GetIpPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    NSUInteger  an_Integer;
+    NSArray * ipItemsArray;
+    NSString *externalIP;
+    
+    NSURL *iPURL = [NSURL URLWithString:@"http://www.dyndns.org/cgi-bin/check_ip.cgi"];
+    
+    if (iPURL) {
+        NSError *error = nil;
+        NSString *theIpHtml = [NSString stringWithContentsOfURL:iPURL
+                                                       encoding:NSUTF8StringEncoding
+                                                          error:&error];
+        if (!error) {
+            NSScanner *theScanner;
+            NSString *text = nil;
+            
+            theScanner = [NSScanner scannerWithString:theIpHtml];
+            
+            while ([theScanner isAtEnd] == NO) {
+                
+                // find start of tag
+                [theScanner scanUpToString:@"<" intoString:NULL] ;
+                
+                // find end of tag
+                [theScanner scanUpToString:@">" intoString:&text] ;
+                
+                // replace the found tag with a space
+                //(you can filter multi-spaces out later if you wish)
+                theIpHtml = [theIpHtml stringByReplacingOccurrencesOfString:
+                             [ NSString stringWithFormat:@"%@>", text]
+                                                                 withString:@" "] ;
+                ipItemsArray =[theIpHtml  componentsSeparatedByString:@" "];
+                an_Integer=[ipItemsArray indexOfObject:@"Address:"];
+                
+                externalIP =[ipItemsArray objectAtIndex:  ++an_Integer];
+                
+                
+                
+            }
+            
+            
+            NSLog(@"%@",externalIP);
+        } else {
+            NSLog(@"Oops... g %d, %@",
+                  [error code],
+                  [error localizedDescription]);
+        }
+    }
+    
+//    [pool drain];
+    result(externalIP);
+    
+}
+
+
 @end
