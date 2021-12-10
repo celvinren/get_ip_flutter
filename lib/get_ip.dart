36940c0775f22ec44349e2b6944f1c2062d80c5c
Calvin <calvin@amaka.com>
2021-12-10 14:25:35 +1100

    null safety support

--- lib/get_ip.dart
+++ lib/get_ip.dart
@@ -10,16 +10,16 @@ class GetIp {
   ///          Can be null.
   ///     iOS: Returns either IPv4- or IPv6-Address.
   ///          Can be null.
-  static Future<String> get ipAddress async {
-    final String ip = await _channel.invokeMethod('getIpAdress');
+  static Future<String?> get ipAddress async {
+    final String? ip = await _channel.invokeMethod('getIpAdress');
     return ip;
   }
 
   ///
   /// Android: returns the users IPv6-Address.
   /// iOS: null
-  static Future<String> get ipv6Address async {
-    final String ip = await _channel.invokeMethod('getIpV6Adress');
+  static Future<String?> get ipv6Address async {
+    final String? ip = await _channel.invokeMethod('getIpV6Adress');
     return ip;
   }
 }
